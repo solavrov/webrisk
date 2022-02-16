95311e98d6f562e7328de834e006d9ac853ab720
solavrov <2slavrov@gmail.com>
2022-02-16 20:40:12 +0300

    sample size picker removed

--- public_html/sample.js
+++ public_html/sample.js
@@ -6,14 +6,14 @@ function resample(glob, forEmpty) {
         glob.html.resampButton.disabled = true;
         glob.html.sampPick.forEach((elem) => elem.disabled = true);
         glob.html.thinker2.style.visibility = "visible";
-        glob.html.thinker3.style.visibility = "visible";
+        //glob.html.thinker3.style.visibility = "visible";
         window.setTimeout(function() {
             for (let c of glob.curList) {
                 glob.data.sample[c] = glob.data.covcc[c].sample(glob.sampleSize).mult(0.01).exp();
             };
             glob.table.port.refreshSummary();
             glob.html.thinker2.style.visibility = "hidden";
-            glob.html.thinker3.style.visibility = "hidden";
+            //glob.html.thinker3.style.visibility = "hidden";
             glob.html.resampButton.disabled = false;
             glob.html.sampPick.forEach((elem) => elem.disabled = false);
         }, 50);
